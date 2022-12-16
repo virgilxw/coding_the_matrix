fceef4ca2a4bd9ba9fcc214245928f7f719add9c
virgilxw <reubenwxw@gmail.com>
2022-12-16 15:40:20 +0000

    geometry lab done

--- part4_The_Matrix/a.py
+++ part4_The_Matrix/a.py
@@ -1,3 +1,6 @@
 from image_mat_util import file2mat,mat2display
-a = file2mat("part4_The_Matrix/a.png")
+from geometry_lab import *
+a = translation(0, 0) * file2mat("part4_The_Matrix/a.png")[0]
+
+# translation(-x,-y) + rotation(theta) * translation(x, y)
 mat2display(a[0], a[1])
