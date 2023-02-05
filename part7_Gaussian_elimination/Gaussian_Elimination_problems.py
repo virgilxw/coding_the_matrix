6118bcf60b609e8a756e0e2bf1d0ea934fa18bdd
virgilxw <reubenwxw@gmail.com>
2023-02-05 15:35:09 +0000

    part 7 completed

--- part7_Gaussian_elimination/Gaussian_Elimination_problems.py
+++ part7_Gaussian_elimination/Gaussian_Elimination_problems.py
@@ -1,35 +1,14 @@
 # version code c2eb1c41017f+
 # Please fill out this stencil and submit using the provided submission script.
 
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 from matutil import *
 from GF2 import one
-
-
-
-## 1: (Problem 7.9.2) Recognizing Echelon Form
-# Write each matrix as a list of row lists
-
-echelon_form_1 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-echelon_form_2 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-echelon_form_3 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-echelon_form_4 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-
+from echelon import transformation
 
 ## 2: (Problem 7.9.3) Is it echelon?
 def is_echelon(A):
@@ -54,18 +33,28 @@ def is_echelon(A):
         True
         >>> is_echelon([[0],[1]])
         False
+        >>> is_echelon([[2,1,0],[0,-4,0],[0,0,1]])
+        True
+        >>> is_echelon([[2,1,1],[-4,0,0],[0,0,1]])
+        False
+        >>> is_echelon([[2,1,1],[0,3,0],[1,0,1]])
+        False
+        >>> is_echelon([[1,1,1,1,1],[0,2,0,1,3],[0,0,0,5,3]])
+        True
     '''
-    pass
 
+    firstNonzero = -1
+    for row in A:
+        newFirstNonZero = next((i for i, x in enumerate(row) if x), None)
+        newFirstNonZero = newFirstNonZero if newFirstNonZero else 0
 
+        if newFirstNonZero > firstNonzero:
+            firstNonzero = newFirstNonZero
+        else:
+            return False
+    return True
 
-## 3: (Problem 7.9.4) Solving with Echelon Form: No Zero Rows
-# Give each answer as a list
-
-echelon_form_vec_a = ...
-echelon_form_vec_b = ...
-echelon_form_vec_c = ...
-
+is_echelon([[1,1,1],[0,1,1],[0,0,1]])
 
 
 ## 4: (Problem 7.9.5) Solving with Echelon Form
@@ -76,7 +65,7 @@ solving_with_echelon_form_a = ...
 solving_with_echelon_form_b = ...
 
 
-
+from vecutil import zero_vec
 ## 5: (Problem 7.9.6) Echelon Solver
 def echelon_solve(rowlist, label_list, b):
     '''
@@ -91,25 +80,69 @@ def echelon_solve(rowlist, label_list, b):
     >>> U_rows = [Vec(D, {'A':one, 'E':one}), Vec(D, {'B':one, 'E':one}), Vec(D,{'C':one})]
     >>> b_list = [one,0,one]
     >>> cols = ['A', 'B', 'C', 'D', 'E']
-    >>> echelon_solve(U_rows, cols, b_list)
-    Vec({'B', 'C', 'A', 'D', 'E'},{'B': 0, 'C': one, 'A': one})
+    >>> echelon_solve(U_rows, cols, b_list) == Vec({'B', 'C', 'A', 'D', 'E'},{'B': 0, 'C': one, 'A': one})
+    True
+
+    >>> b = [one, 0, one, one]
+    >>> D = {'A', 'B', 'C', 'D','E'}
+    >>> rowlist = [Vec(D, {'A': one, 'B':0, 'C':one, 'D':one, 'E':0}), Vec(D, {'A': 0, 'B':one, 'C':0, 'D':0, 'E':one}), Vec(D, {'A': 0, 'B':0, 'C':one, 'D':0, 'E':one}), Vec(D, {'A': 0, 'B':0, 'C':0, 'D':0, 'E':one})]
+    >>> label_list = ['A', 'B', 'C', 'D', 'E']
+    >>> echelon_solve(rowlist, label_list, b) == Vec({'B', 'C', 'A', 'D', 'E'},{'A':one, 'B': one, 'C':0, 'D':0, 'E':one})
+    True
+
+    >>> b = [one, 0, one, 0]
+    >>> D = {'A', 'B', 'C', 'D','E'}
+    >>> rowlist = [Vec(D, {'A': one, 'B':one, 'C':0, 'D':one, 'E':0}), Vec(D, {'A': 0, 'B':one, 'C':0, 'D':one, 'E':one}), Vec(D, {'A': 0, 'B':0, 'C':one, 'D':0, 'E':one}), Vec(D, {'A': 0, 'B':0, 'C':0, 'D':0, 'E':0})]
+    >>> label_list = ['A', 'B', 'C', 'D', 'E']
+    >>> echelon_solve(rowlist, label_list, b) == Vec({'B', 'C', 'A', 'D', 'E'},{'A':one, 'B': 0, 'C':one, 'D':0, 'E':0})
+    True
     '''
-    pass
 
+    D = rowlist[0].D
+    x = zero_vec(D)
+    for j in reversed(range(len(rowlist))):
+        row = rowlist[j]
+        c = next((x for x in label_list if row[x]), None)
+        if c:
+            x[c] = (b[j] - x*row)/row[c]
+        else:
+            continue
+    
+    return x
 
+b = [one, 0, one, 0]
+D = {'A', 'B', 'C', 'D','E'}
+rowlist = [Vec(D, {'A': one, 'B':one, 'C':0, 'D':one, 'E':0}), Vec(D, {'A': 0, 'B':one, 'C':0, 'D':one, 'E':one}), Vec(D, {'A': 0, 'B':0, 'C':one, 'D':0, 'E':one}), Vec(D, {'A': 0, 'B':0, 'C':0, 'D':0, 'E':0})]
+label_list = ['A', 'B', 'C', 'D', 'E']
+echelon_solve(rowlist, label_list, b) == Vec({'B', 'C', 'A', 'D', 'E'},{'A':one, 'B': 0, 'C':one, 'D':0, 'E':0})
 
 ## 6: (Problem 7.9.7) Solving General Matrices via Echelon
-rowlist = [ ... ]    # Provide as a list of Vec instances
-label_list = [ ... ] # Provide as a list
-b = [ ... ]          # Provide as a list of GF(2) values
 
+def solve(A, b):
+    M = transformation(A)
+    U = M*A
+    col_label_list = sorted(A.D[1])
+    U_rows_dict = mat2rowdict(U)
+    row_list = [U_rows_dict[i] for i in sorted(U_rows_dict)]
+    return echelon_solve(row_list,col_label_list, M*b)
 
+def solve_var(A, b):
+    M = transformation(A)
+    U = M*A
+    col_label_list = sorted(A.D[1])
+    U_rows_dict = mat2rowdict(U)
+    row_list = [U_rows_dict[i] for i in sorted(U_rows_dict)]
+    return (row_list,col_label_list, M*b)
 
-## 7: (Problem 7.9.8) Nullspace A
-null_space_rows_a = {...} # Put the row numbers of M from the PDF
+M = Mat(({'a', 'b', 'c', 'd'}, {'A', 'B', 'C', 'D'}), {
+    ('a', 'A'): one, ('a', 'B'): one, ('a', 'D'): one,
+    ('b', 'A'): one, ('b', 'D'): one,
+    ('c', 'A'): one, ('c', 'B'): one, ('c', 'C'): one, ('c', 'D'): one,
+    ('d', 'C'): one, ('d', 'D'): one
+})
+v = Vec(M.D[0], {'a': one, 'c': one})
+(rowlist,col_label_list, mb) = solve_var(M, v)
 
-
-
-## 8: (Problem 7.9.9) Nullspace B
-null_space_rows_b = {...} # Put the row numbers of M from the PDF
-
+rowlist = rowlist   # Provide as a list of Vec instances
+label_list = col_label_list # Provide as a list
+b = [mb[k] for k in sorted(mb.D)]          # Provide as a list of GF(2) values
--- part7_Gaussian_elimination/factoring_lab.py
+++ part7_Gaussian_elimination/factoring_lab.py
@@ -1,6 +1,11 @@
 # version code 80e56511a793+
 # Please fill out this stencil and submit using the provided submission script.
 
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 from vec import Vec
 from GF2 import one
 
@@ -9,10 +14,81 @@ from factoring_support import intsqrt
 from factoring_support import gcd
 from factoring_support import primes
 from factoring_support import prod
+from factoring_support import intsqrt
 
 import echelon
 
-## Task 1
+from math import ceil, sqrt
+
+## 7.8.1
+def root_method(N):
+    '''
+    Initialize integer a to be an integer greater than √N
+• Check if √a2 - N is an integer.
+• If so, let b = √a2 - N. Success! Return a - b.
+• If not, repeat with the next greater value of a.
+    '''
+
+    def find_b(a, N):
+        b = a**2-N
+
+        if intsqrt(b)**2 == b:
+            return a-sqrt(b)
+        else:
+            return find_b(a+1, N)
+
+    a = ceil(sqrt(N))
+    
+    return find_b(a, N)
+
+## 7.8.2
+def gcd(x,y): return x if y == 0 else gcd(y, x % y)
+
+from random import randint
+
+r = randint(100000,999999)
+s = randint(100000,999999)
+t = randint(100000,999999)
+
+a = r*s
+b = s*t
+
+d = gcd(a,b)
+
+if a % d == 0 and b % d ==0 and d >=s:
+    print("7.8.2 succeeded")
+else:
+    print("7.8.2 failed") 
+
+## 7.8.3
+# Let N = 367160330145890434494322103, let a = 67469780066325164, and
+# let b = 9429601150488992, and verify that a ∗ a − b ∗ b is divisible by N. That means that
+# the greatest common divisor of a − b and N has a chance of being a nontrivial divisor of
+# N. Test this using the gcd procedure, and report the nontrivial divisor you found
+
+N = 367160330145890434494322103
+a = 67469780066325164
+b = 9429601150488992
+
+if (a*a - b*b)/N == True:
+    print("axa-bxb is divisible")
+else: 
+    print("axa-bxb is not divisible") 
+
+d = gcd(a-b, N)
+if N/d % 1 == 0:
+    print("divisor is " + str(d))
+
+## 7.8.4
+primeset={2, 3, 5, 7, 11, 13}
+print("dumb_factor of 12 = ",  dumb_factor(12, primeset))
+print("dumb_factor of 154 = ", dumb_factor(154, primeset))
+print("dumb_factor of 2*3*3*3*11*11*13 = ", dumb_factor(2*3*3*3*11*11*13, primeset))
+print("dumb_factor of 2*17 = ", dumb_factor(2*17, primeset))
+print("dumb_factor of 2*3*5*7*19 = ", dumb_factor(2*3*5*7*19, primeset))
+
+
+## Task 1, 7.8.5
 def int2GF2(i):
     '''
     Returns one if i is odd, 0 otherwise.
@@ -28,9 +104,12 @@ def int2GF2(i):
         >>> int2GF2(100)
         0
     '''
-    pass
+    if i % 2 == 1:
+        return one
+    else:
+        return 0
 
-## Task 2
+## Task 2, 7.8.6
 def make_Vec(primeset, factors):
     '''
     Input:
@@ -44,9 +123,9 @@ def make_Vec(primeset, factors):
         >>> make_Vec({2,3,11}, [(2,3), (3,2)]) == Vec({2,3,11},{2:one})
         True
     '''
-    pass
+    return Vec(primeset, {k:int2GF2(i) for (k,i) in factors})
 
-## Task 3
+## Task 3, 7.8.7
 def find_candidates(N, primeset):
     '''
     Input:
@@ -82,11 +161,37 @@ def find_candidates(N, primeset):
                 Vec(D,{2: one, 3: one, 13: one})])
         True
     '''
-    pass
+    roots = []
+    rowlist = []
+
+    x = intsqrt(N)+2
+    xo = x*x - N
+
+    while len(roots) < len(primeset)+1:
+        factored = dumb_factor(xo, primeset)
+        if factored:
+            roots.append(x)
+            rowlist.append(make_Vec(primeset, factored))
+            
+        x = x+1
+        xo = x*x - N
+
+    return roots, rowlist
+
+## 7.8.8
+
+a = 53*77
+b = 2 * 3 ** 2 * 5 * 13
+gcd(a-b,N) == 1
 
 
+# 7.8.9
+a = 52 * 67 * 71
+b = 2 * 3 ** 2 * 5 * 19 * 23
+gcd(a-b,N)
 
-## Task 4
+
+## Task 4 7.8.10
 def find_a_and_b(v, roots, N):
     '''
     Input: 
@@ -107,8 +212,44 @@ def find_a_and_b(v, roots, N):
         >>> find_a_and_b(v, roots, N)
         (4081, 1170)
     '''
-    pass
+
+    alist = [roots[i] for i in range(len(roots)) if v[i] != 0]
+    a = prod(alist)
+    c = prod([x*x-N for x in alist])
+    b = intsqrt(c)
+    assert b*b == c
+    return (a,b)
+
+roots = [51, 52, 53, 58, 61, 62, 63, 67, 68, 71, 77, 79]
+N = 2419
+v = Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},{1: one, 2: one, 11: one, 5: one})  
+find_a_and_b(v, roots, N)
+
+## 7.8.11
+
+def find_divisor (N, primeMax):
+    primeset = set(list(primes(primeMax)))
+
+    roots, rowlist = find_candidates(N, primeset)
+    M = echelon.transformation_rows(rowlist)
+
+    for i in list(range(-len(M), 0))[::-1]:
+        
+        v = M[i]
+        a, b = find_a_and_b(v, roots, N)
+        d = gcd(a-b,N)
+
+        if d != 1:
+            return d
+        else:
+            continue
+
 
 ## Task 5
 
-nontrivial_divisor_of_2461799993978700679 = ... 
+nontrivial_divisor_of_2461799993978700679 = find_divisor(2461799993978700679, 10000)
+print("nontrivial_divisor_of_2461799993978700679 = ", nontrivial_divisor_of_2461799993978700679)
+
+## 7.8.12
+nontrivial_divisor_of_20672783502493917028427 = find_divisor(20672783502493917028427, 10000)
+print("nontrivial_divisor_of_20672783502493917028427 = ", nontrivial_divisor_of_20672783502493917028427) 
